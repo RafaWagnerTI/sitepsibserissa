dd914a530fa05078ef13659363c182d9eb7f0c7b
RafaWagnerTI <rafaelwagner2000@gmail.com>
2024-10-14 01:09:46 -0300

    projeto quase finalizado

--- js/script.js
+++ js/script.js
@@ -15,3 +15,59 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+const questionarios = document.querySelectorAll(".questionario");
+
+function esconderRespostas(questionarios) {
+
+  questionarios.forEach(q => {
+    const pergunta = q.childNodes[1];
+    const icone = pergunta.childNodes[1];
+    q.style.height = `${pergunta.clientHeight + 10}px`;
+    if(!q.classList.contains("fechado")) {
+      q.classList.remove("aberto");
+      q.classList.add("fechado");
+    }
+
+    if(!icone.classList.contains("bi-caret-right-fill")) {
+      icone.classList.remove("bi-caret-down-fill");
+      icone.classList.add("bi-caret-right-fill");
+    }
+  });
+
+};
+
+function atribuirEventoClique(questionarios) {
+
+  questionarios.forEach(q => {
+    const pergunta = q.childNodes[1];
+    const icone = pergunta.childNodes[1];
+    const resposta = q.childNodes[3];
+
+    pergunta.addEventListener('click', e => {
+      const isFechado = q.classList.contains("fechado");
+      q.style.height = isFechado ? `${q.clientHeight + resposta.clientHeight + 10}px` : `${pergunta.clientHeight + 10}px`;
+      q.classList.remove( isFechado ? "fechado" : "aberto");
+      q.classList.add(isFechado ? "aberto" : "fechado");
+      icone.classList.remove(isFechado ? "bi-caret-right-fill" : "bi-caret-down-fill");
+      icone.classList.add(isFechado ? "bi-caret-down-fill" : "bi-caret-right-fill");  
+    });
+  });
+
+}
+
+esconderRespostas(questionarios);
+atribuirEventoClique(questionarios);
+
+window.addEventListener("resize", () => {
+  esconderRespostas(questionarios);
+});
+
+const btnMobile = document.getElementById('menu-mobile');
+
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('aberto');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
